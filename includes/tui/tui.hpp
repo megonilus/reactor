7b5441f26591a0ea7b0eda032cdc7a0283de27fc
megonilus <bibre728@mail.ru>
2025-10-24 16:35:59 +0300

    Layout

--- includes/tui/tui.hpp
+++ includes/tui/tui.hpp
@@ -1,24 +1,95 @@
 #pragma once
 
+#include <common.hpp>
 #include <connect.hpp>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/screen/string.hpp>
+#include <ftxui/component/component_base.hpp>
 
 namespace tui
 {
+	class MainWindow
+	{
+		std::string test_output;
+
+	public:
+		MainWindow(const MainWindow&)			 = default;
+		MainWindow(MainWindow&&)				 = default;
+		MainWindow& operator=(const MainWindow&) = default;
+		MainWindow& operator=(MainWindow&&)		 = default;
+		~MainWindow()							 = default;
+
+		MainWindow() : test_output("Main Window") {}
+
+		ftxui::Component renderer();
+	};
+
+	class ControlWindow
+	{
+		std::string test_output;
+
+	public:
+		ControlWindow(const ControlWindow&)			   = default;
+		ControlWindow(ControlWindow&&)				   = default;
+		ControlWindow& operator=(const ControlWindow&) = default;
+		ControlWindow& operator=(ControlWindow&&)	   = default;
+		~ControlWindow()							   = default;
+
+		explicit ControlWindow(std::string test_output) : test_output(std::move(test_output)) {}
+		ControlWindow() : test_output("Control Window") {}
+
+		ftxui::Component renderer();
+	};
+
+	class StatWindow
+	{
+		std::string test_output;
+
+	public:
+		StatWindow(const StatWindow&)			 = default;
+		StatWindow(StatWindow&&)				 = default;
+		StatWindow& operator=(const StatWindow&) = default;
+		StatWindow& operator=(StatWindow&&)		 = default;
+		~StatWindow()							 = default;
+
+		explicit StatWindow(std::string test_output) : test_output(std::move(test_output)) {}
+		StatWindow() : test_output("Stat Window") {}
+
+		ftxui::Component renderer();
+	};
+
+	class Bar
+	{
+
+		std::vector<std::string> tab_names;
+		int						 tab_selected = 0;
+
+		MainWindow	  main_window;
+		ControlWindow control_window;
+		StatWindow	  stat_window;
+
+	public:
+		ftxui::Component renderer();
+
+		Bar(const Bar&)			   = default;
+		Bar(Bar&&)				   = default;
+		Bar& operator=(const Bar&) = default;
+		Bar& operator=(Bar&&)	   = default;
+		~Bar()					   = default;
+
+		explicit Bar(std::vector<std::string> tab_names) : tab_names(std::move(tab_names)) {}
+		explicit Bar() : tab_names({"main", "control", "stat"}) {};
+	};
+
 	class Instance
 	{
 		std::string name;
 		ConnectPtr	connection;
-
-		void display_bar();
 
 	public:
 		Instance(std::string name, ConnectPtr connection)
 			: name(std::move(name)), connection(std::move(connection))
 		{
 		}
+
 		Instance(const Instance&)			 = delete;
 		Instance(Instance&&)				 = default;
 		Instance& operator=(const Instance&) = delete;
--- src/tui/tui.cpp
+++ src/tui/tui.cpp
@@ -1,4 +1,8 @@
-#include <ftxui/screen/screen.hpp>
+#include <cstdint>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/flexbox_config.hpp>
 #include <tui.hpp>
 
 using namespace tui;
@@ -6,11 +10,94 @@ using namespace ftxui;
 
 void Instance::display()
 {
-	Element document = hbox({
-		text("Hello, World!") | border,
-	});
-	auto	screen	 = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+	auto screen = ScreenInteractive::Fullscreen();
 
-	Render(screen, document);
-	screen.Print();
+	Bar	 bar;
+	auto bar_renderer = bar.renderer();
+
+	auto root = Renderer(bar_renderer, [&] { return vbox({bar_renderer->Render()}) | border; });
+
+	screen.Loop(root);
+}
+
+Component Bar::renderer()
+{
+	auto tab_toggle = Toggle(&tab_names, &tab_selected);
+
+	FlexboxConfig config;
+	config.direction = FlexboxConfig::Direction::Column;
+
+	auto tab_container = Container::Tab(
+		{main_window.renderer() | yflex_grow, control_window.renderer(), stat_window.renderer()
+
+		},
+		&tab_selected);
+
+	auto tab_filled = Renderer(tab_container, [tab_container]
+							   { return tab_container->Render() | yflex | xflex | frame; });
+
+	auto container = Container::Vertical({
+						 tab_toggle,
+						 Renderer([] { return separator(); }),
+						 tab_filled,
+					 }) |
+					 flex;
+
+	return container;
+}
+Component MainWindow::renderer()
+{
+
+	// constexpr uint16_t CONTAINER_WIDTH = 500;
+	// constexpr uint8_t  CONTAINER_HEIGHT = 15;
+	// constexpr uint8_t  WINDOW_RATIO		= CONTAINER_WIDTH / 2;
+
+	auto info_content		= text("Info content");
+	auto connection_content = text("Connection info ");
+
+	auto info2_content	 = text("Info 2 content");
+	auto reactor_content = text("Reactor content");
+
+	FlexboxConfig config;
+	config.direction	   = FlexboxConfig::Direction::Row;
+	config.align_content   = FlexboxConfig::AlignContent::Center;
+	config.align_items	   = FlexboxConfig::AlignItems::Stretch;
+	config.justify_content = FlexboxConfig::JustifyContent::Center;
+
+	FlexboxConfig col_config;
+	col_config.direction	   = FlexboxConfig::Direction::Column;
+	col_config.justify_content = FlexboxConfig::JustifyContent::Stretch;
+	col_config.align_items	   = FlexboxConfig::AlignItems::Stretch;
+	col_config.align_content   = FlexboxConfig::AlignContent::Stretch;
+
+	auto top = flexbox(
+		{
+			window(text("Info"), info_content) | flex,
+			window(text("Connection"), connection_content) | flex,
+		},
+		config);
+
+	auto bot = flexbox({window(text("Info 2"), info2_content) | flex,
+						window(text("Reactor state"), reactor_content) | flex},
+					   config);
+
+	auto layout = flexbox({top | flex, bot | flex}, col_config);
+
+	return Renderer([layout] { return layout; });
+};
+
+Component StatWindow::renderer()
+{
+
+	return Container::Vertical({
+		Renderer([this] { return text(test_output) | border; }),
+	});
+}
+
+Component ControlWindow::renderer()
+{
+
+	return Container::Vertical({
+		Renderer([this] { return text(test_output) | border; }),
+	});
 }
